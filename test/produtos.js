aaca9fc67a09297e08ff26312b580e9ee0f5d755
NathPaiva <hello@nathpaiva.com.br>
2017-04-17 22:48:22 -0300

    create test for create product

--- test/produtos.js
+++ test/produtos.js
@@ -10,4 +10,32 @@ describe('#Produtos Controller', function() {
       .expect(200, done);
   });
 
+  it('#Renderiza o HTML de produto', function(done) {
+    request.get('/produtos')
+      .expect('Content-type', /html/)
+      .expect(200, done);
+  });
+
+  it('#Cadastrado produto invalido', function(done) {
+    request.post('/produtos')
+      .send({
+        titulo: '',
+        descricao: 'novo livro',
+        preco: ''
+      })
+      .set('Accept', 'application/json')
+      .expect('Content-type', /json/)
+      .expect(400, done);
+  });
+
+  it('#Cadastrado produto valido', function(done) {
+    request.post('/produtos')
+      .send({
+        titulo: 'Meu Produto',
+        descricao: 'novo livro',
+        preco: 20.5
+      })
+      .expect(302, done);
+  });
+
 });
