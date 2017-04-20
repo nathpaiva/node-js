b30955b792511779eaed666b56d93ddbb4d9db17
NathPaiva <hello@nathpaiva.com.br>
2017-04-19 22:58:18 -0300

    config for heroku

--- app/infra/connectionFactory.js
+++ app/infra/connectionFactory.js
@@ -15,6 +15,16 @@ var _connectionMYSQL = function() {
     config.database = 'alura_casadocodigo_test';
   }
 
+  if (process.env.NODE_ENV === 'production') {
+    var urlDeConexao = process.env.CLEARDB_DATABASE_URL;
+    // mysql://b0b1f27be5870f:9cd4b731@us-cdbr-iron-east-03.cleardb.net/heroku_c165ec7ef5cd023?reconnect=true
+    var grupos = urlDeConexao.match(/mysql:\/\/(.*):(.*)@(.*)\/(.*)\?reconnect=true/);
+    config.host = grupos[3];
+    config.user = grupos[1];
+    config.password = grupos[2];
+    config.database = grupos[4];
+  }
+
   return mysql.createConnection(config);
 }
 
